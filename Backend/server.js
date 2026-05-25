63c8485314724d71c76e9f6f3115df38921d1cf9
Jayendra Bamne <jayendrabamne68@gmail.com>
2026-05-25 12:42:22 +0530

    frontendURL
    :wq

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://zoronal-task-one.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
